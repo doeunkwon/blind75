1dd2c044a3ab20f3bb55f95663a8e056f0d964c6
doeunkwon <bkwon38@gmail.com>
2022-09-05 00:23:24 +0200

    max_profit.py

---
+++ array/max_profit.py
@@ -0,0 +1,9 @@
+def max_profit(prices):
+    low, high, profit = prices[0], prices[0], 0
+    for i in range(1, len(prices)):
+        if prices[i] < low:
+            low, high = prices[i], prices[i]
+        elif prices[i] > high:
+            high = prices[i]
+        profit = max(high - low, profit)
+    return profit
