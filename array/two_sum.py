e344c4fc6ef057ac00f1b08aab67bb690ae5f738
doeunkwon <bkwon38@gmail.com>
2022-09-04 18:52:07 +0200

    add Array solution

---
+++ array/two_sum.py
@@ -0,0 +1,8 @@
+def twoSum(nums, target):
+    visited = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in visited:
+            return [visited[diff], i]
+        else:
+            visited[num] = i
